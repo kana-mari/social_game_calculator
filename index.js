a50e167977b2e94a862efd01f6024e9b02fc5fe5
kana-mari <54198247+kana-mari@users.noreply.github.com>
2020-08-01 15:50:24 +0900

    変数名を変更

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-// DOM取得
-// ユーザーからの入力を受け取る部分
+// ユーザーからの入力を受け取る
 const
 	getSpan = document.getElementById('js-input_span'),
 	getGoal = document.getElementById('js-input_goal'),
 	getTotal = document.getElementById('js-input_total'),
 	getApprox = document.getElementById('js-input_approx'),
-	getButton = document.getElementById('js-button'),
-	// 計算した結果を出力する部分
-	getRemain = document.getElementById('js-output-remain'),
-	getDailyGoal = document.getElementById('js-output-dailyGoal'),
-	getRemainBattle = document.getElementById('js-output-remainBattle'),
-	getDailyBattle = document.getElementById('js-output-dailyBattle');
+	getButton = document.getElementById('js-button');
+// 計算した結果を出力する部分
+const outputRemain = document.getElementById('js-output-remain'),
+	outputDailyGoal = document.getElementById('js-output-dailyGoal'),
+	outputRemainBattle = document.getElementById('js-output-remainBattle'),
+	outputDailyBattle = document.getElementById('js-output-dailyBattle');
 
 // 関数 フォームへの入力内容を取得→整数にパース→計算→html上で表示する
 const calc = () => {
@@ -27,16 +26,16 @@ const calc = () => {
 		const remain = gameEvent.goal - gameEvent.total, // 目標までのポイント数
 			dailyGoal = Math.round(remain / gameEvent.span); // 1日当たりの目標ポイント
 		// htmlに出力
-		getRemain.textContent = `${remain.toLocaleString()}pt.`;
-		getDailyGoal.textContent = `${dailyGoal.toLocaleString()}pt.`;
+		outputRemain.textContent = `${remain.toLocaleString()}pt.`;
+		outputDailyGoal.textContent = `${dailyGoal.toLocaleString()}pt.`;
 
 		// approxが空欄ではなく、数値が入力されている場合、周回回数系を計算して出力
 		if (getApprox.value !== "" && !Number.isNaN(gameEvent.approx)) {
 			const remainBattle = Math.ceil(remain / gameEvent.approx), // 残り周回回数
 				dailyBattle = Math.ceil(dailyGoal / gameEvent.approx); // 1日当たりの必要出撃回数
 			// htmlに出力
-			getRemainBattle.textContent = `${remainBattle.toLocaleString()}回`;
-			getDailyBattle.textContent = `${dailyBattle.toLocaleString()}回`;
+			outputRemainBattle.textContent = `${remainBattle.toLocaleString()}回`;
+			outputDailyBattle.textContent = `${dailyBattle.toLocaleString()}回`;
 		}
 	} else { // フォームへの入力内容が数値でない場合
 		alert('整数を入力してください。');
