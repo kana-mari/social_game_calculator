49cefe2ee0108814c6b54d59355674933b318213
kana-mari <54198247+kana-mari@users.noreply.github.com>
2020-08-14 21:22:58 +0900

    getElementByID修正

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-console.log(v8n().number().test(1000));
-
 // 計算ボタン押下時関数を実行
 document.getElementById('js-button').addEventListener('click', () => {
 	// 関数 フォームへの入力内容を取得→整数にパース→計算→html上で表示する
-	// ユーザーからの入力を受け取る
-	const
-		getSpan = document.getElementById('js-input_span'),
-		getGoal = document.getElementById('js-input_goal'),
-		getTotal = document.getElementById('js-input_total'),
-		getApprox = document.getElementById('js-input_approx');
 	// 入力内容を取得
 	const gameEvent = {
-		span: Number.parseInt(getSpan.value, 10), // イベント日数
-		goal: Number.parseInt(getGoal.value, 10), // 目標pt.
-		total: Number.parseInt(getTotal.value, 10), // 獲得済みpt.
-		approx: Number.parseInt(getApprox.value, 10) // 1周回あたりの平均獲得pt.
+		span: parseInt(document.getElementById('js-input_span').value, 10), // イベント日数
+		goal: parseInt(document.getElementById('js-input_goal').value, 10), // 目標pt.
+		total: parseInt(document.getElementById('js-input_total').value, 10), // 獲得済みpt.
+		approx: parseInt(document.getElementById('js-input_approx').value, 10) // 1周回あたりの平均獲得pt.
 	};
 	// input#span, goal, totalをNaN判定
-	if (!Number.isNaN(gameEvent.goal && gameEvent.goal && gameEvent.total)) {
+	if (!isNaN(gameEvent.span && gameEvent.goal && gameEvent.total)) {
 		// 数値の場合、計算
 		const remain = gameEvent.goal - gameEvent.total, // 目標までのポイント数
 			dailyGoal = Math.round(remain / gameEvent.span); // 1日当たりの目標ポイント
@@ -32,7 +24,7 @@ document.getElementById('js-button').addEventListener('click', () => {
 		outputDailyGoal.textContent = `${dailyGoal.toLocaleString()}pt.`;
 
 		// approxが空欄ではなく、数値が入力されている場合、周回回数系を計算して出力
-		if (getApprox.value !== "" && !Number.isNaN(gameEvent.approx)) {
+		if (gameEvent.approx !== "" && !isNaN(gameEvent.approx)) {
 			const remainBattle = Math.ceil(remain / gameEvent.approx), // 残り周回回数
 				dailyBattle = Math.ceil(dailyGoal / gameEvent.approx); // 1日当たりの必要出撃回数
 			// htmlに出力
