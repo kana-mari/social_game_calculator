a85b65c15b108084c5e9fa32f72602f4531e1184
kana-mari <54198247+kana-mari@users.noreply.github.com>
2020-08-16 21:22:46 +0900

    権利表記追加

--- index.js
+++ index.js
@@ -49,3 +49,8 @@ document.getElementById('js-button').addEventListener('click', () => {
 		alert('整数を入力してください。');
 	}
 });
+
+// 権利表示
+/* v8n v1.3.3
+Copyright (c) 2018 Bruno C. Couto
+https://github.com/imbrn/v8n/blob/master/LICENSE */
