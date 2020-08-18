cfc1b3dca7dc48256043130d65c7544a0822136e
kana-mari <54198247+kana-mari@users.noreply.github.com>
2020-08-18 20:51:40 +0900

    変数と処理を変更

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // 計算ボタン押下時関数を実行
 document.getElementById('js-button').addEventListener('click', () => {
 	// 関数 フォームへの入力内容を取得→整数にパース→計算→html上で表示する
-	// 入力内容を取得
-	const gameEvent = {
+	// 入力内容を取得、数値に変換
+	const getInput = {
 		span: parseInt(document.getElementById('js-input_span').value, 10), // イベント日数
 		goal: parseInt(document.getElementById('js-input_goal').value, 10), // 目標pt.
 		total: parseInt(document.getElementById('js-input_total').value, 10), // 獲得済みpt.
@@ -15,14 +15,14 @@ document.getElementById('js-button').addEventListener('click', () => {
 		.integer();
 	if (validateForm
 		.greaterThanOrEqual(1)
-		.test(gameEvent.span, gameEvent.goal)
+		.test(getInput.span, getInput.goal)
 		&&
 		validateForm
 			.greaterThanOrEqual(0)
-			.test(gameEvent.total)) {
+			.test(getInput.total)) {
 		// 数値の場合、計算
-		const remain = gameEvent.goal - gameEvent.total, // 目標までのポイント数
-			dailyGoal = Math.round(remain / gameEvent.span); // 1日当たりの目標ポイント
+		const calcRemain = getInput.goal - getInput.total, // 目標までのポイント数
+			calcDailyGoal = Math.round(calcRemain / getInput.span); // 1日当たりの目標ポイント
 		// htmlに出力
 		// 出力先DOM取得
 		const outputRemain = document.getElementById('js-output-remain'),
@@ -30,18 +30,21 @@ document.getElementById('js-button').addEventListener('click', () => {
 			outputRemainBattle = document.getElementById('js-output-remainBattle'),
 			outputDailyBattle = document.getElementById('js-output-dailyBattle');
 		// 計算結果を出力
-		outputRemain.textContent = `${remain.toLocaleString()}pt.`;
-		outputDailyGoal.textContent = `${dailyGoal.toLocaleString()}pt.`;
+		outputRemain.textContent = `${calcRemain.toLocaleString()}pt.`;
+		outputDailyGoal.textContent = `${calcDailyGoal.toLocaleString()}pt.`;
 
 		// approxが空欄ではなく、数値が入力されている場合、周回回数系を計算して出力
 		if (validateForm
 			.greaterThanOrEqual(1)
-			.test(gameEvent.approx)) {
-			// htmlに出力
-			// 残り戦闘回数
-			outputRemainBattle.textContent = `${Math.ceil(remain / gameEvent.approx).toLocaleString()}回`;
-			// 1日当たりの必要出撃回数
-			outputDailyBattle.textContent = `${Math.ceil(dailyGoal / gameEvent.approx).toLocaleString()}回`;
+			.test(getInput.approx)) {
+			// 残り戦闘回数を計算して出力
+			outputRemainBattle.textContent = `${Math.ceil(calcRemain / getInput.approx).toLocaleString()}回`;
+			// 1日当たりの必要出撃回数を計算して出力
+			outputDailyBattle.textContent = `${Math.ceil(calcDailyGoal / getInput.approx).toLocaleString()}回`;
+		} else {
+			// 空の内容を出力
+			outputRemainBattle.textContent,
+				outputDailyBattle.textContent = "";
 		}
 	} else { // フォームへの入力内容が数値でない場合
 		alert('整数を入力してください。');
