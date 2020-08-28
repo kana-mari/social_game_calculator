6bea3d7bbf59d3e49112f1f73b687a8674719f4d
kana-mari <54198247+kana-mari@users.noreply.github.com>
2020-08-28 16:05:11 +0900

    submitイベントをpreventDefaultするテスト

--- index-test.js
+++ index-test.js
@@ -1,8 +1,5 @@
-import v8n from "./lib/v8n/v8n.js";
-const validationRule = v8n().numeric().integer();
-console.log(validationRule.test([1, 2, 3]));
-v8n()
-	.numeric()
-	.integer()
-	.greaterThanOrEqual(1)
-	.test(1, 2);
+// submitイベントしてページ遷移をさせたくない
+document.getElementById('form').addEventListener('submit', () => {
+	console.log(document.getElementById('input-text').value);
+	event.preventDefault();
+});
