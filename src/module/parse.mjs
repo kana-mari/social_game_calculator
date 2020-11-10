248f1385be29f212c3493c3b4427e2b05f47d274
kana-mari <54198247+kana-mari@users.noreply.github.com>
2020-11-10 20:38:52 +0900

    引数名変更

--- src/module/parse.mjs
+++ src/module/parse.mjs
@@ -4,12 +4,12 @@ import v8n from "https://unpkg.com/v8n/dist/v8n.esm.js";
 
 /**
  * パースする関数
- * @param params {string} - inputのvalue
+ * @param arg {string} - inputのvalue
  * @returns {number} - 引数をパースした数値
  */
-const parse = function parseInput(params) {
-	if (v8n().string().pattern(/\d+/).test(params)) {
-		return parseInt(params, 10);
+const parse = function parseInput(arg) {
+	if (v8n().string().pattern(/\d+/).test(arg)) {
+		return parseInt(arg, 10);
 	} else {
 		throw new Error('引数が数値で構成された文字列でない');
 	}
