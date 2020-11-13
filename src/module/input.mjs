fc9823720108f143860659ef07d0191c0b17415e
kana-mari <54198247+kana-mari@users.noreply.github.com>
2020-11-13 21:38:04 +0900

    inputのテスト

--- src/module/input.mjs
+++ src/module/input.mjs
@@ -1,9 +1,38 @@
 import { } from "./validate.mjs";
+import { parse, parseNullable } from "./parse.mjs";
 
-/** 
- * inputのvalueをまとめたMap
- * @type {Map}
-*/
-const input = new Map;
-const hogehoge = mapのkey;
-input.set(hogehoge, document.getElementById(`js-${hogehoge}`).value);
+/**
+ * 
+ */
+const input = function getInputAndParse() {
+	/**
+	 * mapのkeyをまとめた配列
+	 * @type {Array}
+	 */
+	const keyArray = [span, goal, total, approx];
+	// getElementを繰り返す
+	const span = document.getElementById(`js-input_${keyArray[0]}`);
+	const goal = document.getElementById(`js-input_${keyArray[1]}`);
+	const total = document.getElementById(`js-input_${keyArray[2]}`);
+	const approx = document.getElementById(`js-input_${keyArray[3]}`);W
+
+	if (approx以外がOKな値か) {
+		/**
+		 * input要素のvalueをまとめたマップ
+		 * @type {map}
+		 */
+		const inputValues = new Map;
+		inputValues.set(hogehoge, document.getElementById(`js-input_${hogehoge}`).value);
+
+		if (approxの有無とOKな値か) {
+			// mapにapproxをset
+		}
+		return inputValues;
+
+	} else {
+		// approx以外がNGな値の場合、例外を投げる
+		throw new Error('input要素の値が正しくない');
+	}
+};
+
+export default input;
--- test/test-input.mjs
+++ test/test-input.mjs
@@ -1,25 +1,31 @@
 // eslint-disable-next-line no-undef
 const assert = chai.assert; // ←chaiはindex.htmlでグローバルに読み込む
 
-import { } from "./../src/module/input.mjs";
+import input from "./../src/module/input.mjs";
 
 // Red
 /*
 - 特に何も渡さなくてもinputのvalueをまとめたmapを返す関数
 - valueがダメな値だったら例外を返す
-	└ ダメな値→string型以外
+	- OK => string && 数字で構成された文字列
+	- ダメな値 => string型以外
 - mapは2パターン approxの有無で分ける
-- テストランナーにテスト用input作る
 */
 
 describe('input系のテスト 正常系', () => {
-	it('', () => {
-		assert();
-	})
+	it('inputのvalueが数字で構成された文字列→Mapを返す', () => {
+		assert.instanceOf(input(), Map);
+	});
+	it('approxに記入した場合、Map.sizeが4を返す', () => {
+		assert.strictEqual(input().size, 4);
+	});
+	it('approxが空欄の場合、Map.sizeが3を返す', () => {
+		assert.strictEqual(input().size, 3);
+	});
 });
 
 describe('input系のテスト 例外系', () => {
-	it('', () => {
-		assert.throws(() => { }, Error);
-	})
+	it('input要素のvalueに数値にパース出来ない文字を入力', () => {
+		assert.throws(() => { input() }, Error);
+	});
 });
