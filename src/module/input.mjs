40e7781ec1069863a87c5f43a0b1aee6d96e08b5
kana-mari <54198247+kana-mari@users.noreply.github.com>
2020-11-24 20:54:30 +0900

    修正

--- src/module/input.mjs
+++ src/module/input.mjs
@@ -1,53 +1,70 @@
 // モジュールインポート
-import { isPosiInt, isPosiIntAnd0 } from "./validate.mjs";
+import { isPosiInt, isPosiIntAnd0, isBlank } from "./validate.mjs";
 import Parse from "./parse.mjs";
 
 /**
- * 
+ * Parseをインスタンス化
  */
 const parse = new Parse();
 
-// マップ関連まとめたい
-const Hogehoge = class makeParsedValueMap { };
-
 /**
- * Mapのキーになる値をまとめた配列
- * @type {Array}
+ * input内容をまとめたMapを作成するクラス
  */
-const keysArray = ['span', 'goal', 'total', 'approx'];
+const MakeMap = class makeParsedValueMap {
+	constructor() {
+		/**
+		* Mapのキーになる値をまとめた配列
+		* @type {Array}
+		*/
+		const keys = ['span', 'goal', 'total', 'approx'];
 
-/**
- * パースした値をまとめたMap
- * @type {Map<string, number | null>}
- */
-const parsedValueMap = new Map;
-// span, goal, totalをパースした値をset
-for (let i = 0; i <= 2; i++) {
-	parsedValueMap.set(keysArray[i], parse.parseInput(document.getElementById(`js-input_${keysArray[i]}`).value));
-}
-// approxはparse.allowBlankしてset
-parsedValueMap.set(keysArray[3], parse.allowBlank(document.getElementById(`js-input_${keysArray[3]}`).value));
+		/**
+		 * パースした値をまとめたMap
+		 * @type {Map<string, number | null>}
+		 */
+		const parsedValueMap = new Map;
 
-/**
- * Mapの内容が正しいかチェックする関数
- * @param map {Map} - 入力内容をまとめたMap
- */
-const input = function getInputAndParse(map) {
-	// span, goal, totalが正しいかどうかチェック
-	if (isPosiInt.test([map.get('span'), map.get('goal')]) && isPosiIntAnd0.test(map.get('total'))) {
-		// approxがnullならapproxを削除
-		if (map.get('approx') === null) {
-			map.delete('approx');
+		// 値をset
+		// span, goal, totalをパースした値をset
+		for (let i = 0; i <= 2; i++) {
+			parsedValueMap.set(keys[i], parse.parseInput(document.getElementById(`js-input_${keys[i]}`).value));
 		}
-		return map;
-	} else {
-		// approx以外がNGな値の場合、例外を投げる
-		throw new Error('入力値が正しくない');
+
+		/**
+		 * approxの入力内容をget
+		 */
+		const approx = document.getElementById('js-input_approx').value;
+		// 空欄でなければapproxをset
+		if (!isBlank.test(approx)) {
+			parsedValueMap.set(keys[3], parse.parseInput(document.getElementById(`js-input_${keys[3]}`).value));
+		}
+		return parsedValueMap;
+	}
+};
+
+/**
+ * Mapの内容が正しいかチェックする
+ * @param map {Map} - input内容をまとめたMap
+ * @returns {Map} - 
+ */
+const validateInput = function validateInputMap(map) {
+	// map.sizeが3なら3つチェック、4なら4つチェック
+	// 3・4以外なら例外を投げる
+	switch (map.size) {
+		case 3:
+			isPosiInt.test([map.get('span'), map.get('goal')]) && isPosiIntAnd0.test(map.get('total'));
+			break;
+
+		case 4:
+			isPosiInt.test([map.get('span'), map.get('goal'), map.get('approx')]) && isPosiIntAnd0.test(map.get('total'));
+			break;
+
+		default:
+			throw new Error('入力値が正しくない');
 	}
 };
 
 // テスト用のエクスポート
-export { keysArray, parsedValueMap };
+export { MakeMap };
 
 // 本番用のエクスポート
-export default input;
