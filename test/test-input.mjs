24db863fd63d5626c6d56cf4d878e11303725686
kana-mari <54198247+kana-mari@users.noreply.github.com>
2020-11-26 21:26:57 +0900

    テストコード変更

--- test/test-input.mjs
+++ test/test-input.mjs
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-undef
 const assert = chai.assert; // ←chaiはindex.htmlでグローバルに読み込む
 
-import { MakeMap, checkMap } from "./../src/module/input.mjs";
+import { InputMap, checkMap } from "./../src/module/input.mjs";
 
 // Green
 
 // インスタンスの初期化
-const inputMap = new MakeMap();
+const inputMap = new InputMap();
 
 describe('MakeMapのテスト（正常系）', () => {
 	it('Mapかどうかチェック', () => {
@@ -41,14 +41,14 @@ describe('CheckMapのテスト（正常系）', () => {
 	it('size=3のMap(values=number)を渡す→trueを返す', () => {
 		assert.isTrue(checkMap(testMapSize3True));
 	});
-	it('size=4のMap(values=number)を渡す→真偽値を返す', () => {
+	it('size=4のMap(values=number)を渡す→trueを返す', () => {
 		assert.isTrue(checkMap(testMapSize4True));
 	});
-	it('size=3のMap(values!=number)を渡す→真偽値を返す', () => {
-		assert.isBoolean(checkMap(testMapSize3False));
+	it('size=3のMap(values!=number)を渡す→falseを返す', () => {
+		assert.isFalse(checkMap(testMapSize3False));
 	});
 	it('size=4のMap(values!=number)を渡す→真偽値を返す', () => {
-		assert.isBoolean(checkMap(testMapSize4False));
+		assert.isFalse(checkMap(testMapSize4False));
 	});
 });
 
@@ -56,28 +56,36 @@ describe('CheckMapのテスト（例外系）', () => {
 	/** 
 	 * Map.size !== 3 || 4
 	 * typeof value !== number
-	 * value === NaN
-	 * value === infinity
+	 * value === NaN || infinity
 	 * keyが変
 	 **/
 	it('sizeが3・4でないMapを渡す', () => {
 		assert.throws(() => {
-			checkMap(), RangeError
+			checkMap(new Map([['span', 7]])), RangeError
 		});
 	});
-	it('', () => {
+	it('valueがnumberでないMapを渡す', () => {
 		assert.throws(() => {
-			checkMap(), RangeError
+			checkMap(testMapSize3False), RangeError
 		});
 	});
-	it('', () => {
+	it('keyにspan, goal, totalを含まないMap(size=3)を渡す', () => {
 		assert.throws(() => {
-			checkMap(), RangeError
+			checkMap(new Map([
+				['hogehoge', 1],
+				['hogehoge2', 2],
+				['hogehoge3', 3],
+			])), RangeError
 		});
 	});
-	it('', () => {
+	it('keyにspan, goal, total,approxを含まないMap(size=4)を渡す', () => {
 		assert.throws(() => {
-			checkMap(), RangeError
+			checkMap(new Map([
+				['hogehoge', 1],
+				['hogehoge2', 2],
+				['hogehoge3', 3],
+				['hogehoge4', 4]
+			])), RangeError
 		});
 	});
 });
