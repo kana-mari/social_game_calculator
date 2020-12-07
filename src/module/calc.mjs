c45e3f114e889ce2afc4c0a2f325aa0c045538b3
kana-mari <54198247+kana-mari@users.noreply.github.com>
2020-12-07 22:13:59 +0900

    calc関数追加

---
+++ src/module/calc.mjs
@@ -0,0 +1,31 @@
+/**
+ * 計算する関数
+ * @param map {Map<string, number>}
+ * @returns {Map<string, number>}
+ */
+const calc = function calcNumber(map) {
+	if (/** 引数をチェック **/) {
+
+
+
+	} else {
+		// ダメな場合、例外を投げる
+		throw new Error('引数が正しくない');
+	}
+};
+
+// 返り値(4項目)
+new Map([
+	[],
+	[],
+	[],
+	[]
+]);
+
+// 返り値(2項目)
+new Map([
+	[],
+	[]
+]);
+
+export default calc;
