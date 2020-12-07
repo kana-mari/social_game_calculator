342e0c3b0c4be229d27aa795ee1b5f3c08468f4a
kana-mari <54198247+kana-mari@users.noreply.github.com>
2020-12-07 22:13:24 +0900

    テスト内容追加

--- test/test-calc.mjs
+++ test/test-calc.mjs
@@ -1,4 +1,41 @@
 // eslint-disable-next-line no-undef
 const assert = chai.assert; // ←chaiはindex.htmlでグローバルに読み込む
 
-import {  } from "./../src/module/calc.mjs";
+import calc from "./../src/module/calc.mjs";
+
+/**
+ * # テストすべき項目
+ * ## 正常系
+ * - 返り値がMapか
+ * - Mapのvaluesがnumber型か
+ * ## 例外系
+ * - Map以外を渡したときに例外を返すか
+ **/
+
+describe('正常系', () => {
+	const testMap3True = new Map([['span', 7], ['goal', 1000], ['total', 100]]);
+	const testMap4True = new Map(testMap3True);
+	testMap4True.set('approx', 100);
+
+	it('Mapを渡すとMapを返す', () => {
+		assert.instanceOf();
+	});
+});
+
+describe('例外系', () => {
+	it('', () => {
+		assert();
+	});
+});
+
+describe('', () => {
+	it('', () => {
+		assert();
+	});
+});
+
+describe('', () => {
+	it('', () => {
+		assert();
+	});
+});
