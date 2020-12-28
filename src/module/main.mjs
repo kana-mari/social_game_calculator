8e880c2ac7ebff53f54b713f6a910ee3bc77d846
kana-mari <54198247+kana-mari@users.noreply.github.com>
2020-12-28 21:15:53 +0900

    domParseモジュールを追加、エラーブロック表示周りを修正

--- src/module/main.mjs
+++ src/module/main.mjs
@@ -1,13 +1,14 @@
 /**
  * @file 各モジュールを統合
  * @todo バンドラーでまとめる
- * @todo エラーをDOMに表示
+ * @todo クラスにまとめる
  */
 
 // モジュールをインポート
 import Input from "./input.mjs";
 import calc from "./calc.mjs";
 import Output from "./outputHtml.mjs";
+import domParse from "./domParse.mjs";
 
 /**
  * エラー表示用のoutput要素 #js-output_error
@@ -21,20 +22,36 @@ document.getElementById('js-form').addEventListener('submit', (event) => {
 		// form要素のデフォルト動作をキャンセル
 		event.preventDefault();
 
-		// エラー表示ブロックを非表示
+		// エラー表示をリセット
+		// エラーブロック非表示
 		outputError.classList.add('error--invisible');
+		// エラー表示ブロックの中身をすべて削除
+		while (outputError.firstChild) {
+			outputError.removeChild(outputError.firstChild);
+		}
+
+		/**
+		 * 入力内容 メーター表示に使い回すため変数に格納
+		 * @type {Map<string, number>}
+		 */
+		const input = new Input();
 
 		/**
 		 * 計算結果 メーター表示に使い回すため変数に格納
 		 * @type {Map<string, number>}
 		 */
-		const result = calc(new Input());
+		const result = calc(input);
+
 		new Output(result);
 	} catch (error) {
-		// エラー表示
+		// コンソールにエラー表示
 		console.error(error);
+		// エラーブロック可視化
 		outputError.classList.remove('error--invisible');
+		// DOMをパースしてoutputErrorの子要素
+		outputError.appendChild(domParse(`<h1 class="error__title">！エラーが発生しました。</h1>`));
+		outputError.appendChild(domParse(`<p>入力内容を確認してください。<br>半角数字のみ入力できます。</p>`));
 	}
 });
 
-export default
+// export default main;
