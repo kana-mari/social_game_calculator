4fc05cbbda1c2c057c1616d73df17ab99ebbcdca
kana-mari <54198247+kana-mari@users.noreply.github.com>
2021-01-17 22:29:28 +0900

    webpackでの開発環境と本番環境の設定分離かい

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,15 +1,16 @@
 // 開発用設定ファイル
-module.exports = {
+
+// 共通設定
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.config.common.js');
+
+module.exports = merge(commonConfig,{
 	// モードの設定
 	mode: 'development',
-	// エントリーポイント
-	entry: './src/index.js',
 
 	// ファイルの出力設定
 	output: {
-		//  出力ファイルのディレクトリ名
-		path: `${__dirname}/src`,
 		// 出力ファイル名
 		filename: 'bundle.js'
 	}
-};
+});
